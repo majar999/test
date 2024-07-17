3afb6d18ef357b586bf638c3a6780c7603572751
majar999 <majarozic999@gmail.com>
2024-07-17 03:12:21 +0200

    Update FEtest2

--- FrontendTests/FEtest2.py
+++ FrontendTests/FEtest2.py
@@ -39,7 +39,7 @@ class CoinMarketCapTest:
             print("Otvaranje CoinMarketCap veb stranice.")
             self.driver.get("https://coinmarketcap.com")
             WebDriverWait(self.driver, 30).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "cmc-table-row"))
+                EC.presence_of_element_located((By.CSS_SELECTOR, "#__next > div.sc-2e66506f-1.buMEwe.global-layout-v2 > div.main-content > div.cmc-body-wrapper > div > div:nth-child(1) > div.sc-963bde9f-2.bZVSBs > table"))
             )
             print("Veb stranica uspešno otvorena.")
         except Exception as e:
@@ -51,19 +51,31 @@ class CoinMarketCapTest:
         """
         try:
             print(f"Biranje {num_to_add} nasumičnih kriptovaluta za dodavanje na watchlist.")
-            crypto_rows = WebDriverWait(self.driver, 30).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME, "cmc-table-row"))
+            table = WebDriverWait(self.driver, 30).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "#__next > div.sc-2e66506f-1.buMEwe.global-layout-v2 > div.main-content > div.cmc-body-wrapper > div > div:nth-child(1) > div.sc-963bde9f-2.bZVSBs > table"))
             )
+            crypto_rows = table.find_elements(By.TAG_NAME, "tr")
             selected_indices = random.sample(range(len(crypto_rows)), num_to_add)
             for index in selected_indices:
-                elipsis_button = crypto_rows[index].find_element(By.CSS_SELECTOR, "button.cmc-link")
-                self.driver.execute_script("arguments[0].scrollIntoView();", elipsis_button)
-                elipsis_button.click()
+                add_to_watchlist_selector = "#__next > div.sc-2e66506f-1.buMEwe.global-layout-v2 > div.main-content > div.cmc-body-wrapper > div > div:nth-child(1) > div.sc-963bde9f-2.bZVSBs > table > tbody > tr:nth-child(3) > td:nth-child(1) > span > span"
                 add_to_watchlist = WebDriverWait(self.driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Add to Watchlist')]"))
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, add_to_watchlist_selector))
                 )
-                add_to_watchlist.click()
+                self.driver.execute_script("arguments[0].scrollIntoView();", add_to_watchlist)
+                WebDriverWait(self.driver, 10).until(
+                    EC.visibility_of(add_to_watchlist)
+                )
+                self.driver.execute_script("arguments[0].click();", add_to_watchlist)
                 time.sleep(2)  # Kratko kašnjenje kako bi se osiguralo da je stavka dodata
+                
+                # Zatvaranje popup prozora
+                close_popup_selector = "body > div.sc-acb6320-0.jPvGvf.modalOpened > div > div > div > svg"
+                close_popup = WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, close_popup_selector))
+                )
+                close_popup.click()
+                time.sleep(1)  # Kratko kašnjenje kako bi se osiguralo da je popup zatvoren
+                
             print(f"{num_to_add} kriptovaluta dodata na watchlist.")
         except Exception as e:
             print(f"Došlo je do greške prilikom dodavanja na watchlist: {e}")
@@ -73,11 +85,19 @@ class CoinMarketCapTest:
         Otvara watchlist u novom tabu.
         """
         try:
+            print("Skrolovanje stranice do vrha.")
+            self.driver.execute_script("window.scrollTo(0, 0);")
+            time.sleep(2)  # Kratko kašnjenje kako bi se osiguralo da je stranica skrolovana do vrha
+
             print("Otvaranje watchlist u novom tabu.")
-            self.driver.execute_script("window.open('https://coinmarketcap.com/watchlist', '_blank');")
+            watchlist_button_selector = "#__next > div.sc-2e66506f-1.buMEwe.global-layout-v2 > div.main-content > div.sc-2e66506f-0.dfkLgL > div.sc-d8ea16bb-0.hZhyvu > div:nth-child(3) > div > div.sc-d1ede7e3-0.sc-5d9fc2ce-0.bwRagp.eeQQNU.right-section-link-buttons > div > button > a > span"
+            watchlist_button = WebDriverWait(self.driver, 30).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, watchlist_button_selector))
+            )
+            watchlist_button.click()
             self.driver.switch_to.window(self.driver.window_handles[1])
             WebDriverWait(self.driver, 30).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME, "cmc-table-row"))
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#__next > div.sc-2e66506f-1.buMEwe.global-layout-v2 > div.main-content > div.cmc-body-wrapper > div > div:nth-child(1) > div.sc-963bde9f-2.bZVSBs > table"))
             )
             print("Watchlist uspešno otvorena.")
         except Exception as e:
@@ -92,7 +112,7 @@ class CoinMarketCapTest:
         try:
             print(f"Verifikacija {num_to_verify} kriptovaluta na watchlist.")
             watchlist_rows = WebDriverWait(self.driver, 30).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME, "cmc-table-row"))
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#__next > div.sc-2e66506f-1.buMEwe.global-layout-v2 > div.main-content > div.cmc-body-wrapper > div > div:nth-child(1) > div.sc-963bde9f-2.bZVSBs > table"))
             )
             if len(watchlist_rows) >= num_to_verify:
                 print("Sve izabrane kriptovalute su na watchlist.")
